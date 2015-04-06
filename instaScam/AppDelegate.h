7a6ad7aa8f8d7e4d97e777bbf8f888870b0b9ce7
TJMueller <timothyjonmueller@gmail.com>
2015-04-06 16:26:20 -0500

    Initial Commit

---
+++ instaScam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instaScam
+//
+//  Created by Timothy Mueller on 4/6/15.
+//  Copyright (c) 2015 Timothy Mueller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instaScam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  instaScam
+//
+//  Created by Timothy Mueller on 4/6/15.
+//  Copyright (c) 2015 Timothy Mueller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
