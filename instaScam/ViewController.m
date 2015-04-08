818329e2408bce563a99e163059b8bc8695440be
Ben Whitley <blw305@gmail.com>
2015-04-08 11:32:46 -0500

    testing merge

--- instaScam/ViewController.m
+++ instaScam/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by Timothy Mueller on 4/6/15.
 //  Copyright (c) 2015 Timothy Mueller. All rights reserved.
 //
-// to do set up segues
+// to do set up segues DAVE TEST
 
 
 #import "ViewController.h"
