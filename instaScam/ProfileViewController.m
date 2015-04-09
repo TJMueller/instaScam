ca53fbeb02e7e124f3a29620fd11324d871b4b50
TJMueller <timothyjonmueller@gmail.com>
2015-04-09 09:50:29 -0500

    profile picture

--- instaScam/ProfileViewController.m
+++ instaScam/ProfileViewController.m
@@ -8,6 +8,7 @@
 
 #import "ProfileViewController.h"
 #import <Parse/Parse.h>
+#import "Person.h"
 
 @interface ProfileViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *profilePicImageView;
@@ -26,6 +27,28 @@
     // Do any additional setup after loading the view.
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    PFUser *user = [PFUser currentUser];
+    PFQuery *query = [Person query];
+    NSLog(@"%@", user.username);
+    [query whereKey:@"userName" equalTo:user.username];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        Person *person = objects[0];
+        self.followersLabel.text = [NSString stringWithFormat:@"Followers: %lu", (unsigned long)person.followers.count];
+        self.followingLabel.text = [NSString stringWithFormat:@"Following: %lu", (unsigned long)person.following.count];
+        self.postsLabel.text = [NSString stringWithFormat:@"Posts: %lu", (unsigned long)person.posts.count];
+        [person.profilePic getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            if (!error) {
+                self.profilePicImageView.image = [UIImage imageWithData:data];
+            } else {
+                NSLog(@"%@", error);
+            }
+        }];
+    }];
+
+    self.fullNameLabel.text = user[@"fullName"];
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
